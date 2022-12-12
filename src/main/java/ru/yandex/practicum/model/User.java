94b58f163b55812f8c6b6cf56b558ef60c010ce9
BagratPUH <avanesianbag@gmail.com>
2022-12-12 14:36:23 +0300

    feat: Добавлена модель сущности User

---
+++ src/main/java/ru/yandex/practicum/model/User.java
@@ -0,0 +1,23 @@
+package ru.yandex.practicum.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NonNull;
+
+import java.time.LocalDate;
+
+@Data
+@Builder
+@AllArgsConstructor
+public class User {
+    private final int id;
+    @NonNull
+    private String email;
+    @NonNull
+    private String login;
+    @NonNull
+    private String name;
+    @NonNull
+    private final LocalDate birthday;
+}
