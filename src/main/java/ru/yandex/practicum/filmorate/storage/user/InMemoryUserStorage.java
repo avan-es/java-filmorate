6d2df36a8f1eb5eb6c68075be791a929c1522cac
BagratPUH <avanesianbag@gmail.com>
2022-12-28 03:06:47 +0300

    feat: Добавить класс UserValidation. Перенести соответствующие методы.

--- src/main/java/ru/yandex/practicum/filmorate/storage/user/InMemoryUserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/user/InMemoryUserStorage.java
@@ -7,6 +7,7 @@ import ru.yandex.practicum.filmorate.model.User;
 import java.time.LocalDate;
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.Map;
 
 @Component
 public class InMemoryUserStorage implements UserStorage{
@@ -17,10 +18,14 @@ public class InMemoryUserStorage implements UserStorage{
 
     @Override
     public User addUser(User user) {
-        User validUser = userValidation(user);
-        validUser.setId(idUserGenerator);
-        users.put(idUserGenerator++, validUser);
-        return validUser;
+        user.setId(idUserGenerator);
+        users.put(idUserGenerator++, user);
+        return user;
+    }
+
+    @Override
+    public User getUser(int id) {
+        return users.get(id);
     }
 
     @Override
@@ -29,26 +34,7 @@ public class InMemoryUserStorage implements UserStorage{
             throw new UserValidationException("Пользователь с таким ID (" + user.getId() +
                     ") не найден.");
         }
-        User validUser = userValidation(user);
-        users.put(validUser.getId(), validUser);
-        return validUser;
-    }
-
-    @Override
-    public User userValidation(User user) {
-        if (user.getEmail().isEmpty() ||
-                user.getEmail().isBlank() || !user.getEmail().contains("@")) {
-            throw new UserValidationException("Электронная почта не может быть пустой и должна содержать символ @");
-        }
-        if (user.getLogin().isEmpty() || user.getLogin().contains(" ")) {
-            throw  new UserValidationException("Логин не может быть пустым и содержать пробелы.");
-        }
-        if (user.getName() == null || user.getName().isBlank()) {
-            user.setName(user.getLogin());
-        }
-        if (user.getBirthday().isAfter(LocalDate.now())) {
-            throw new UserValidationException("Дата рождения не может быть в будущем.");
-        }
+        users.put(user.getId(), user);
         return user;
     }
 
@@ -63,7 +49,7 @@ public class InMemoryUserStorage implements UserStorage{
     }
 
     @Override
-    public Collection<User> getAllUsers() {
-        return users.values();
+    public Map<Integer, User> getAllUsers() {
+        return users;
     }
 }
