9d320f5a62573fff753d03b5558885b0c96e9cb9
BagratPUH <avanesianbag@gmail.com>
2022-12-28 03:08:47 +0300

    feat: Убрать метод filmValidation и добавить getFilmById

--- src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
@@ -1,16 +1,19 @@
 package ru.yandex.practicum.filmorate.storage.film;
 import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.model.User;
+
 import java.util.Collection;
+import java.util.Map;
 
 public interface FilmStorage {
 
-    Collection<Film> getAllFilms();
-
-    Film filmValidation(Film film);
+    Map<Integer, Film> getAllFilms();
 
     Film addFilm(Film film);
 
     Film updateFilm(Film film);
 
     Film deleteFilm (Film film);
+
+    Film getFilmById(Integer id);
 }
