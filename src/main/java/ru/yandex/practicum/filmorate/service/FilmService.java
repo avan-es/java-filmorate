adffd806b314064f6919264e8bb23cc3ffd639f3
BagratPUH <avanesianbag@gmail.com>
2022-12-28 03:02:59 +0300

    feat: Добавить класс FilmService

---
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
@@ -0,0 +1,33 @@
+package ru.yandex.practicum.filmorate.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.model.User;
+import ru.yandex.practicum.filmorate.storage.film.FilmStorage;
+import ru.yandex.practicum.filmorate.storage.user.UserStorage;
+
+import java.util.Map;
+
+@Service
+@RequiredArgsConstructor
+public class FilmService {
+    private final FilmStorage filmStorage;
+
+
+    public Map<Integer, Film> getAllFilms() {
+        return filmStorage.getAllFilms();
+    }
+
+    public Film addFilm(Film film) {
+        return filmStorage.addFilm(film);
+    }
+
+    public Film updateFilm(Film film) {
+        return filmStorage.updateFilm(film);
+    }
+
+    public Film getFilmById(Integer id) {
+        return filmStorage.getFilmById(id);
+    }
+}
