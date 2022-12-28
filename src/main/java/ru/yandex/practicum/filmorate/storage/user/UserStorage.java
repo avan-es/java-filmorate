d49ffd4170b371b836b9b7aeec60c8dedf5e57b5
BagratPUH <avanesianbag@gmail.com>
2022-12-28 03:04:35 +0300

    feat: Добавить метод getUser. Изменить возращаемую коллекцию в методе getAllUsers

--- src/main/java/ru/yandex/practicum/filmorate/storage/user/UserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/user/UserStorage.java
@@ -1,16 +1,18 @@
 package ru.yandex.practicum.filmorate.storage.user;
 import ru.yandex.practicum.filmorate.model.User;
 import java.util.Collection;
+import java.util.Map;
 
 public interface UserStorage {
 
-    Collection<User> getAllUsers();
-
-    User userValidation(User user);
+    Map<Integer, User> getAllUsers();
 
     User addUser(User user);
 
+    User getUser(int id);  //TZ 10
+
     User updateUser(User user);
 
-    User deleteUser (User user);
+    User deleteUser (User user); //TZ 10
+
 }
