259c0c863d0b18497a865ae7d3d570d4f4ac9bcc
BagratPUH <avanesianbag@gmail.com>
2022-12-28 03:09:53 +0300

    feat: Добавить эндпойнт для getFilmById. Рефакторинг

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,34 +1,44 @@
 package ru.yandex.practicum.filmorate.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.model.Film;
-import ru.yandex.practicum.filmorate.storage.film.FilmStorage;
+import ru.yandex.practicum.filmorate.model.User;
+import ru.yandex.practicum.filmorate.service.FilmService;
+import ru.yandex.practicum.filmorate.validation.FilmValidation;
 
 import javax.validation.Valid;
 import java.util.Collection;
+import java.util.Map;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/films")
 public class FilmController {
-    FilmStorage inMemoryFilmStorage;
+    private final FilmService filmService;
 
-    public FilmController(FilmStorage inMemoryFilmStorage) {
-        this.inMemoryFilmStorage = inMemoryFilmStorage;
-    }
+    private final FilmValidation filmValidation;
 
     @GetMapping
     public Collection<Film> getAllFilms() {
-        return inMemoryFilmStorage.getAllFilms();
+        return filmService.getAllFilms().values();
+    }
+
+    @GetMapping("/{id}")
+    public Film geFilmById(@PathVariable Integer id) {
+        return filmService.getFilmById(id);
     }
 
     @PostMapping
     public Film addFilm(@Valid @RequestBody Film film){
-        return inMemoryFilmStorage.addFilm(film);
+        filmValidation.filmValidation(film);
+        return filmService.addFilm(film);
     }
 
     @PutMapping
     public Film updateFilm(@Valid @RequestBody Film film){
-        return inMemoryFilmStorage.updateFilm(film);
+        filmValidation.filmValidation(film);
+        return filmService.updateFilm(film);
     }
 
 }
