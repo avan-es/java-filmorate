c51471635610b34c45dca621d3bfa42fc14252d5
Bagrat <bagratavanesian@yandex.ru>
2023-02-01 17:32:42 +0300

    fix: sprint11: Назначить нужный бин через @Qualifier("filmDbStorage").

--- src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
@@ -1,6 +1,7 @@
 package ru.yandex.practicum.filmorate.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.storage.film.FilmStorage;
@@ -13,7 +14,8 @@ import java.util.stream.Collectors;
 @Service
 @RequiredArgsConstructor
 public class FilmService {
-    private final FilmStorage filmStorage;
+    @Qualifier("filmDbStorage")
+    private FilmStorage filmStorage;
 
 
     public Map<Integer, Film> getAllFilms() {
