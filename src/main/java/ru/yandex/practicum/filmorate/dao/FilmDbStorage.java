ed6f6fa6708d014e62d53ac2867375ff5d29d725
Bagrat <bagratavanesian@yandex.ru>
2023-02-01 15:30:43 +0300

    feat: sprint11: Создать DAO имплементацию для FilmStorage.

---
+++ src/main/java/ru/yandex/practicum/filmorate/dao/FilmDbStorage.java
@@ -0,0 +1,33 @@
+package ru.yandex.practicum.filmorate.dao;
+
+import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.storage.film.FilmStorage;
+
+import java.util.Map;
+
+public class FilmDbStorage implements FilmStorage {
+    @Override
+    public Map<Integer, Film> getAllFilms() {
+        return null;
+    }
+
+    @Override
+    public Film addFilm(Film film) {
+        return null;
+    }
+
+    @Override
+    public Film updateFilm(Film film) {
+        return null;
+    }
+
+    @Override
+    public Film deleteFilm(Film film) {
+        return null;
+    }
+
+    @Override
+    public Film getFilmById(Integer id) {
+        return null;
+    }
+}
