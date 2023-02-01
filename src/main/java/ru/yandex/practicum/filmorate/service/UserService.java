e70f43a07753677a0a9098367cf0845dc0686933
Bagrat <bagratavanesian@yandex.ru>
2023-02-01 17:23:58 +0300

    feat: sprint11: Назначить нужный бин через     @Qualifier("userDbStorage").

--- src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
@@ -1,6 +1,8 @@
 package ru.yandex.practicum.filmorate.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.storage.user.UserStorage;
@@ -12,8 +14,11 @@ import java.util.stream.Collectors;
 @Service
 @RequiredArgsConstructor
 public class UserService {
+    @Qualifier("userDbStorage")
+    private UserStorage userStorage;
 
-    private final UserStorage userStorage;
+
+
 
     public void addFriend (Integer id, Integer friendId){
         userStorage.getUser(id).getFriends().add(friendId);
