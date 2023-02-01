650d3b7245b7cce5d05e4785778c4a08a2a446a7
Bagrat <bagratavanesian@yandex.ru>
2023-02-01 17:34:26 +0300

    feat: sprint11: Назвать компонент @Component("filmDbStorage").

--- src/main/java/ru/yandex/practicum/filmorate/dao/FilmDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/dao/FilmDbStorage.java
@@ -1,10 +1,12 @@
 package ru.yandex.practicum.filmorate.dao;
 
+import org.springframework.stereotype.Component;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.storage.film.FilmStorage;
 
 import java.util.Map;
 
+@Component("filmDbStorage")
 public class FilmDbStorage implements FilmStorage {
     @Override
     public Map<Integer, Film> getAllFilms() {
