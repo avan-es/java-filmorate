0b920a6dcbe75faf4d9a82bf569cf896d3c35c0e
Bagrat <bagratavanesian@yandex.ru>
2023-02-01 17:24:30 +0300

    feat: sprint11: Задать имя компонента в аннотации @Component.

--- src/main/java/ru/yandex/practicum/filmorate/dao/UserDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/dao/UserDbStorage.java
@@ -1,10 +1,13 @@
 package ru.yandex.practicum.filmorate.dao;
 
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
 import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.storage.user.UserStorage;
 
 import java.util.Map;
 
+@Component("userDbStorage")
 public class UserDbStorage implements UserStorage {
     @Override
     public Map<Integer, User> getAllUsers() {
