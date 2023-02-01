117fa035159f018205ebe37b5f40c4daa40a3968
Bagrat <bagratavanesian@yandex.ru>
2023-02-01 17:26:58 +0300

    feat: sprint11: Задать имя компонента в аннотации @Component.

--- src/main/java/ru/yandex/practicum/filmorate/storage/user/InMemoryUserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/user/InMemoryUserStorage.java
@@ -8,7 +8,7 @@ import ru.yandex.practicum.filmorate.model.User;
 import java.util.HashMap;
 import java.util.Map;
 
-@Component
+@Component("inMemoryUserStorage")
 public class InMemoryUserStorage implements UserStorage{
 
     private final HashMap<Integer, User> users = new HashMap<>();
