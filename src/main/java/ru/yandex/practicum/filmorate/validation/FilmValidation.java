73433273b9102c4ca3037a8d614b48d05f90126e
Bagrat <bagratavanesian@yandex.ru>
2023-02-04 17:48:08 +0300

    fix: sprint11: Добавить валидацию фильма по ID для БД.

--- src/main/java/ru/yandex/practicum/filmorate/validation/FilmValidation.java
+++ src/main/java/ru/yandex/practicum/filmorate/validation/FilmValidation.java
@@ -1,17 +1,23 @@
 package ru.yandex.practicum.filmorate.validation;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
 import org.springframework.stereotype.Component;
 import ru.yandex.practicum.filmorate.exeptions.FilmExeptions.FilmNotFoundException;
 import ru.yandex.practicum.filmorate.exeptions.FilmExeptions.FilmValidationException;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.storage.film.FilmStorage;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.time.LocalDate;
 import java.time.Month;
 
-@RequiredArgsConstructor
+//@RequiredArgsConstructor
 @Component("filmValidation")
 public class FilmValidation {
     @Qualifier("filmDbStorage")
@@ -19,6 +25,11 @@ public class FilmValidation {
 
     public static final LocalDate FILMS_BIRTHDAY = LocalDate.of(1895, Month.DECEMBER, 28);
 
+    private final JdbcTemplate jdbcTemplate;
+    @Autowired
+    public FilmValidation(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
 
     public Film filmValidation(Film film) {
         if (film.getName().isEmpty() ||
@@ -40,6 +51,15 @@ public class FilmValidation {
 
     public void filmIdValidation(int id) {
         if (!filmStorage.getAllFilms().containsKey(id)) {
+            throw new FilmNotFoundException("Фильм с ID: " + id +" не найден.");
+        }
+    }
+
+    public void filmIdValidationDB(int id) {
+        String sqlRequest =  "select * " +
+                "from PUBLIC.FILM where FILM_ID = " + id;
+        SqlRowSet filmRows = jdbcTemplate.queryForRowSet(sqlRequest);
+        if (!filmRows.next()) {
             throw new FilmNotFoundException("Фильм с ID: " + id +" не найден.");
         }
     }
