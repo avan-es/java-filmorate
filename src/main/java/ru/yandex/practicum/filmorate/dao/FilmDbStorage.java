84c4cd14f5fe75818c52abf96c9c652e4606ac3a
Bagrat <bagratavanesian@yandex.ru>
2023-02-04 17:51:05 +0300

    fix: sprint11: Убрать валидацию фильма по ID из метода getFilmById.

--- src/main/java/ru/yandex/practicum/filmorate/dao/FilmDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/dao/FilmDbStorage.java
@@ -6,6 +6,7 @@ import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.support.rowset.SqlRowSet;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
+import ru.yandex.practicum.filmorate.exeptions.FilmExeptions.FilmNotFoundException;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.storage.film.FilmStorage;
 
@@ -13,6 +14,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.time.LocalDate;
 import java.util.Map;
+import java.util.Optional;
 
 @Component("filmDbStorage")
 @Repository
@@ -48,31 +50,23 @@ public class FilmDbStorage implements FilmStorage {
 
     @Override
     public Film getFilmById(Integer id) {
-        System.out.println("Мы в FilmDbStorage");
-        SqlRowSet filmRows = jdbcTemplate.queryForRowSet("select * " +
-                "from PUBLIC.FILM where FILM_ID = ?", id);
-        System.out.println(filmRows.findColumn("RELEASE_DATE"));
-        System.out.println(filmRows.findColumn("FILM_RATING"));
-        if (filmRows.next()) {
+        String sqlRequest =  "select * " +
+        "from PUBLIC.FILM where FILM_ID = " + id;
+        SqlRowSet filmRows = jdbcTemplate.queryForRowSet(sqlRequest);
             // вы заполните данные пользователя в следующем уроке
-            System.out.println("Есть данные");
-        } else {
-            System.out.println("Данные закончились или их нет");
-        }
-        Film lastName = jdbcTemplate.queryForObject(
-                "select * from PUBLIC.FILM where FILM_ID = 1",
-                new RowMapper<Film>() {
-                    public Film mapRow(ResultSet rs, int rowNum) throws SQLException {
-                    Film film = new Film(rs.getInt("FILM_ID"),
-                            rs.getString("film_name"),
-                            rs.getString("film_description"),
-                    LocalDate.parse(rs.getString("release_date")),
-                    rs.getInt("film_duration"),
-                    rs.getInt("film_rating"));
-                    return film;
-                }
-                });
-            return lastName;
-
+            Film film = jdbcTemplate.queryForObject(
+                    sqlRequest,
+                    new RowMapper<Film>() {
+                        public Film mapRow(ResultSet rs, int rowNum) throws SQLException {
+                            Film film = new Film(rs.getInt("FILM_ID"),
+                                    rs.getString("film_name"),
+                                    rs.getString("film_description"),
+                                    LocalDate.parse(rs.getString("release_date")),
+                                    rs.getInt("film_duration"),
+                                    rs.getInt("film_rating"));
+                            return film;
+                        }
+                    });
+            return film;
     }
 }
