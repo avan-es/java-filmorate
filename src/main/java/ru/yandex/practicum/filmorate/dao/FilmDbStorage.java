9ec75b39dc8dc305b26b9d735144f0efe9009a5b
Bagrat <bagratavanesian@yandex.ru>
2023-02-06 00:48:44 +0300

    feat: sprint11: Добавить методы для обратобки запросов: getAllFilms, addFilm, deleteFilm, getFilmById, makeFilm.

--- src/main/java/ru/yandex/practicum/filmorate/dao/FilmDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/dao/FilmDbStorage.java
@@ -3,6 +3,8 @@ package ru.yandex.practicum.filmorate.dao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.jdbc.support.rowset.SqlRowSet;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
@@ -10,6 +12,7 @@ import ru.yandex.practicum.filmorate.exeptions.FilmExeptions.FilmNotFoundExcepti
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.storage.film.FilmStorage;
 
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.time.LocalDate;
@@ -30,7 +33,7 @@ public class FilmDbStorage implements FilmStorage {
     @Override
     public Map<Integer,Film> getAllFilms() {
         List<Film> films = new ArrayList<>();
-        String sqlRequest =  "select * from PUBLIC.FILM";
+        String sqlRequest =  "select * from PUBLIC.FILMS";
         SqlRowSet filmRows = jdbcTemplate.queryForRowSet(sqlRequest);
         films =  jdbcTemplate.query(sqlRequest, (rs, rowNum) -> makeFilm(rs));
         HashMap<Integer, Film> filmsMap = new HashMap<>();
@@ -43,7 +46,22 @@ public class FilmDbStorage implements FilmStorage {
 
     @Override
     public Film addFilm(Film film) {
-        return null;
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        jdbcTemplate.update(connection -> {
+            PreparedStatement ps = connection.prepareStatement("insert into FILMS (FILM_NAME, FILM_DESCRIPTION, RELEASE_DATE, FILM_DURATION, FILM_GENRE_ID, MPA_ID)" +
+                            " VALUES (?, ?, ?, ?, ?)",
+                    new String[]{"FILM_ID"});
+            ps.setString(1, film.getName());
+            ps.setString(2, film.getDescription());
+            ps.setDate(3, java.sql.Date.valueOf(film.getReleaseDate()));
+            ps.setInt(4, film.getDuration());
+            ps.setInt(5, Integer.parseInt(film.getGenre()));
+            //ps.setInt(6, Integer.parseInt(film.getGenre()));
+            return ps;
+        }, keyHolder);
+
+        film.setId(Objects.requireNonNull(keyHolder.getKey()).intValue());
+        return film;
     }
 
     @Override
@@ -53,13 +71,16 @@ public class FilmDbStorage implements FilmStorage {
 
     @Override
     public Film deleteFilm(Film film) {
+        String sqlRequest =  "DELETE " +
+                "from PUBLIC.FILM where FILM_ID = " + film.getId();
+        jdbcTemplate.queryForRowSet(sqlRequest);
         return null;
     }
 
     @Override
     public Film getFilmById(Integer id) {
         String sqlRequest =  "select * " +
-        "from PUBLIC.FILM where FILM_ID = " + id;
+        "from PUBLIC.FILMS where FILM_ID = " + id;
         SqlRowSet filmRows = jdbcTemplate.queryForRowSet(sqlRequest);
         return jdbcTemplate.queryForObject(
                 sqlRequest, new RowMapper<Film>() {
@@ -76,7 +97,8 @@ public class FilmDbStorage implements FilmStorage {
                 rs.getString("film_description"),
                 LocalDate.parse(rs.getString("release_date")),
                 rs.getInt("film_duration"),
-                rs.getInt("film_rating"));
+                rs.getString("film_genre_id"),
+                rs.getString("MPA_ID"));
         return film;
     }
 }
