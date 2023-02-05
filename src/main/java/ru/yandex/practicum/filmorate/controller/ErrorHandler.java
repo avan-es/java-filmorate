75d72c1e1c80b0755f2eb470ac13b6b2866576f5
Bagrat <bagratavanesian@yandex.ru>
2023-02-06 00:36:09 +0300

    feat: sprint11: Добавить handler для ошибки при работе с друзьями.

--- src/main/java/ru/yandex/practicum/filmorate/controller/ErrorHandler.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/ErrorHandler.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import ru.yandex.practicum.filmorate.exeptions.FilmExeptions.FilmNotFoundException;
+import ru.yandex.practicum.filmorate.exeptions.UserExeptions.AddToFriendsException;
 import ru.yandex.practicum.filmorate.exeptions.UserExeptions.UserNotFoundException;
 import ru.yandex.practicum.filmorate.exeptions.UserExeptions.UserValidationException;
 
@@ -24,6 +25,14 @@ public class ErrorHandler {
     public ErrorResponse handleUserValidationException(final UserValidationException e) {
         return new ErrorResponse(
                 "Ошибка в одном из полей.",
+                e.getMessage());
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleAddToFriendsException(final AddToFriendsException e) {
+        return new ErrorResponse(
+                "Ошибка при добавлении в друзья.",
                 e.getMessage());
     }
 
