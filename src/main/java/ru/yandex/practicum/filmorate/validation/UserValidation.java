8f6b9a4c1e6c4255b7a49773f3ab249676d6122b
Bagrat <bagratavanesian@yandex.ru>
2023-02-06 00:39:35 +0300

    feat: sprint11: Добавить валидацию пользователей для работы с БД.

--- src/main/java/ru/yandex/practicum/filmorate/validation/UserValidation.java
+++ src/main/java/ru/yandex/practicum/filmorate/validation/UserValidation.java
@@ -1,7 +1,9 @@
 package ru.yandex.practicum.filmorate.validation;
 
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
 import org.springframework.stereotype.Component;
 import ru.yandex.practicum.filmorate.exeptions.UserExeptions.UserNotFoundException;
 import ru.yandex.practicum.filmorate.exeptions.UserExeptions.UserValidationException;
@@ -11,12 +13,17 @@ import ru.yandex.practicum.filmorate.storage.user.UserStorage;
 import java.time.LocalDate;
 
 
-@RequiredArgsConstructor
 @Component("userValidation")
 public class UserValidation {
 
     @Qualifier("userDbStorage")
     private UserStorage userStorage;
+
+    private final JdbcTemplate jdbcTemplate;
+    @Autowired
+    public UserValidation(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
 
     public User userValidation(User user) {
         if (user.getEmail().isEmpty() ||
@@ -38,6 +45,51 @@ public class UserValidation {
     public void userIdValidation(int id) {
         if (!userStorage.getAllUsers().containsKey(id)) {
             throw new UserNotFoundException("Пользователь с ID: " + id +" не найден.");
+        }
+    }
+
+    public void userIdValidationDB(int id) {
+        String sqlRequest =  "select * " +
+                "from PUBLIC.USERS where USER_ID = " + id;
+        SqlRowSet userRows = jdbcTemplate.queryForRowSet(sqlRequest);
+        if (!userRows.next()) {
+            throw new UserNotFoundException("Пользователь с ID: " + id +" не найден.");
+        }
+    }
+
+    public void userLoginIsBusy (User user) {
+        String sqlRequest =  "select LOGIN " +
+                "from PUBLIC.USERS where LOGIN = " + user.getLogin();
+        try {
+            SqlRowSet userRows = jdbcTemplate.queryForRowSet(sqlRequest);
+            if (userRows.next()) {
+                throw new UserValidationException("Логин: " + user.getLogin() +" уже занят.");
+            }
+        } catch (Exception e){
+
+        }
+    }
+
+    public void userEmailIsBusy (User user) {
+        String sqlRequest =  "select EMAIL " +
+                "from PUBLIC.USERS where EMAIL = " + user.getEmail();
+        SqlRowSet userRows = jdbcTemplate.queryForRowSet(sqlRequest);
+        if (userRows.next()) {
+            throw new UserValidationException("Почта: " + user.getEmail() +" уже занята.");
+        }
+    }
+
+    public boolean isUserPresent(User user){
+        String sqlRequest =  "select USER_ID " +
+                "from PUBLIC.USERS where USER_ID = " + user.getId();
+        try {
+            SqlRowSet userRows = jdbcTemplate.queryForRowSet(sqlRequest);
+            if (!userRows.next()) {
+                throw new UserNotFoundException("Пользователь с ID: " + user.getId() +" не найден.");
+            }
+            return true;
+        } catch (Exception e){
+            throw new UserNotFoundException("Пользователь с ID: " + user.getId() +" не найден.");
         }
     }
 
