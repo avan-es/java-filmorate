2d51d098d927a007cc6c0b91174484c655311ada
Bagrat <bagratavanesian@yandex.ru>
2023-02-06 00:38:13 +0300

    feat: sprint11: Добавить новые поля для объекта и рефакторинг.

--- src/main/java/ru/yandex/practicum/filmorate/storage/user/UserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/user/UserStorage.java
@@ -1,17 +1,15 @@
 package ru.yandex.practicum.filmorate.storage.user;
 import ru.yandex.practicum.filmorate.model.User;
+import java.util.List;
 import java.util.Map;
 
 public interface UserStorage {
-
     Map<Integer, User> getAllUsers();
-
     User addUser(User user);
-
-    User getUser(int id);  //TZ 10
-
+    User getUserById(Integer id);
     User updateUser(User user);
-
-    User deleteUser (User user); //TZ 10
-
+    String addFriend(Integer id, Integer friendId);
+    List<User> getFriends(Integer id);
+    List<User> getCommonFriends(Integer id, Integer otherId);
+    String deleteFriend(Integer id, Integer friendId);
 }
