7811144b1324b96cd09e258b7b0b2b223a7e1ef6
Bagrat <bagratavanesian@yandex.ru>
2023-02-07 01:10:00 +0300

    fix: sprint11: Перенести бизнес-логику в DAO.

--- src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
@@ -8,33 +8,26 @@ import ru.yandex.practicum.filmorate.dao.FilmDbStorage;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.storage.film.FilmStorage;
 import ru.yandex.practicum.filmorate.validation.FilmValidation;
+import ru.yandex.practicum.filmorate.validation.UserValidation;
 
 import java.util.*;
-import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class FilmService {
-    //@Qualifier("filmDbStorage")
     private FilmStorage filmStorage;
 
     private final FilmValidation filmValidation;
+    private final UserValidation userValidation;
 
     @Autowired
     public void setJdbcFilmDAO(@Qualifier("filmDbStorage") FilmDbStorage filmDbStorage) {
         this.filmStorage = filmDbStorage;
     }
 
-    public Map<Integer, Film> getAllFilms() {
-        return filmStorage.getAllFilms();
-    }
-
     public Film addFilm(Film film) {
+        filmValidation.filmValidation(film);
         return filmStorage.addFilm(film);
-    }
-
-    public Film updateFilm(Film film) {
-        return filmStorage.updateFilm(film);
     }
 
     public Film getFilmById(Integer id) {
@@ -42,27 +35,28 @@ public class FilmService {
         return filmStorage.getFilmById(id);
     }
 
+    public Map<Integer, Film> getAllFilms() {
+        return filmStorage.getAllFilms();
+    }
+
+    public Film updateFilm(Film film) {
+        filmValidation.filmIdValidationDB(film.getId());
+        return filmStorage.updateFilm(film);
+    }
+
     public void addLike(int id, int userId) {
-        filmStorage.getFilmById(id).getLikes().add(userId);
-        System.out.println("Hi");
+        filmValidation.filmIdValidationDB(id);
+        userValidation.userIdValidationDB(userId);
+        filmStorage.putLike(id, userId);
     }
 
     public void deleteLike(int id, int userId) {
-        filmStorage.getFilmById(id).getLikes().remove(userId);
+        filmValidation.filmIdValidationDB(id);
+        userValidation.userIdValidationDB(userId);
+        filmStorage.deleteLike(id, userId);
     }
 
     public Set<Film> getTopFilms(Integer count) {
-        Set <Film> topFilms = new TreeSet<>((o1, o2) -> {
-            if(o1.getLikes().size() == o2.getLikes().size()) {
-                return o1.getId() - o2.getId();
-
-            } else if (o1.getLikes().size() > o2.getLikes().size()) {
-                return -1;
-        } else {
-            return 1;
-        }
-    });
-        topFilms.addAll(filmStorage.getAllFilms().values());
-        return topFilms.stream().limit(count).collect(Collectors.toSet());
+        return filmStorage.getTopFilms(count);
     }
 }
