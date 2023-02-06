c41a7a5fe66126283e395fc5bf8cb7b8505936d2
Bagrat <bagratavanesian@yandex.ru>
2023-02-07 01:54:41 +0300

    fix: sprint11: Перенести DAO классы в пакет dao/impl.

--- src/main/java/ru/yandex/practicum/filmorate/dao/impl/UserDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/dao/impl/UserDbStorage.java
@@ -1,4 +1,4 @@
-package ru.yandex.practicum.filmorate.dao;
+package ru.yandex.practicum.filmorate.dao.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
