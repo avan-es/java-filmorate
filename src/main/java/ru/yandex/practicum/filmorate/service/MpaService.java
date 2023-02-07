ed302bf1b224f1d313f6af8adc11466806c08418
Bagrat <bagratavanesian@yandex.ru>
2023-02-07 03:40:09 +0300

    fix: sprint11: Рефакторинг.

--- src/main/java/ru/yandex/practicum/filmorate/service/MpaService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/MpaService.java
@@ -3,14 +3,10 @@ package ru.yandex.practicum.filmorate.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 import ru.yandex.practicum.filmorate.dao.MpaDao;
-import ru.yandex.practicum.filmorate.dao.impl.UserDbStorage;
 import ru.yandex.practicum.filmorate.model.Mpa;
-import ru.yandex.practicum.filmorate.storage.user.UserStorage;
 import ru.yandex.practicum.filmorate.validation.MpaValidation;
-import ru.yandex.practicum.filmorate.validation.UserValidation;
 
 import java.util.List;
 
@@ -23,7 +19,7 @@ public class MpaService {
     private final MpaValidation mpaValidation;
 
     @Autowired
-    public void setJdbcmpaDao(@Qualifier("mpaDaoImpl") MpaDao mpaDao) {
+    public void setJdbcMpaDao(@Qualifier("mpaDaoImpl") MpaDao mpaDao) {
         this.mpaDao = mpaDao;
     }
 
