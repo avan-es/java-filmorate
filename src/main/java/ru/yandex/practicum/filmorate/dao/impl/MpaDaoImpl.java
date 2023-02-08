2236be0da17b119fbf0f6b3ace91b9e0b72d3587
Bagrat <bagratavanesian@yandex.ru>
2023-02-08 03:21:06 +0300

    fix: sprint11: after 1st check: Выгружать MPA одним запросом.

--- src/main/java/ru/yandex/practicum/filmorate/dao/impl/MpaDaoImpl.java
+++ src/main/java/ru/yandex/practicum/filmorate/dao/impl/MpaDaoImpl.java
@@ -1,13 +1,17 @@
 package ru.yandex.practicum.filmorate.dao.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.ResultSetExtractor;
 import org.springframework.jdbc.support.rowset.SqlRowSet;
 import org.springframework.stereotype.Component;
 import ru.yandex.practicum.filmorate.dao.MpaDao;
 import ru.yandex.practicum.filmorate.exeptions.NotFoundException;
 import ru.yandex.practicum.filmorate.model.Mpa;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -38,11 +42,25 @@ public class MpaDaoImpl implements MpaDao {
     @Override
     public List<Mpa> getAllMpas() {
         List<Mpa> result = new ArrayList<>();
-        String sql = "SELECT MPAS_ID FROM MPAS ORDER BY MPAS_ID";
-        List<Integer> ids = jdbcTemplate.queryForList(sql, Integer.class);
+        String sql = "SELECT * FROM MPAS ORDER BY MPAS_ID";
+        result = jdbcTemplate.query(sql, new ResultSetExtractor<List<Mpa>>() {
+            @Override
+            public List<Mpa> extractData(ResultSet rs) throws SQLException, DataAccessException {
+                List<Mpa> listMpa = new ArrayList<>();
+                while (rs.next()){
+                    Mpa mpa = new Mpa();
+                    mpa.setId(rs.getInt("MPAS_ID"));
+                    mpa.setName(rs.getString("MPAS_NAME"));
+                    listMpa.add(mpa);
+                }
+
+                return listMpa;
+            }
+        });
+       /* List<Integer> ids = jdbcTemplate.queryForList(sql, Integer.class);
         for (Integer id : ids) {
             result.add(getMpaById(id));
-        }
+        }*/
         return result;
     }
 }
