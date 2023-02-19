abc8569a04aa82629a9eb1ab349c742be24b967a
Bagrat <bagratavanesian@yandex.ru>
2023-02-19 16:28:40 +0300

    fix: myOwn: Рефакторинг.

--- src/main/java/ru/yandex/practicum/filmorate/model/Director.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Director.java
@@ -2,11 +2,17 @@ package ru.yandex.practicum.filmorate.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+
 
 @Data
 @AllArgsConstructor
+@RequiredArgsConstructor
 public class Director {
     private Integer id;
+    @NotBlank
     private String name;
 }
 
